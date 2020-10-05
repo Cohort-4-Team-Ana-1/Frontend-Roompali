ec1382cd9477fa3580dfef9121209cf8b81b9a61
Johán Suárez <32972612+JohanSuarezQ@users.noreply.github.com>
2020-10-04 22:02:35 -0500

    [FIXED] unnecessary folder

--- src/pages/ScreenHome.js
+++ src/pages/ScreenHome.js
@@ -3,7 +3,6 @@ import { HomeHero } from "../components/HomeHero";
 
 export const ScreenHome = () => {
   const city = localStorage.getItem("city");
-  console.log(city);
   return (
     <main>
       <HomeHero />
